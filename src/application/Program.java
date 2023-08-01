75c5481671525ae04d2b5a046a985cd6ad1b4358
Bruno Pires <brunopires.ctto@gmail.com>
2023-08-01 11:44:30 -0300

    Set - Demo3

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,29 +1,22 @@
 package application;
 
-import java.util.Arrays;
 import java.util.Set;
 import java.util.TreeSet;
 
+import entities.Product;
+
 public class Program {
 	public static void main(String[] args) {
-		
-		Set<Integer> a = new TreeSet<>(Arrays.asList(0,2,4,5,6,8,10));
-		Set<Integer> b = new TreeSet<>(Arrays.asList(5,6,7,8,9,10));
-	
-		//union (UNIÃO)
-		Set<Integer> c = new TreeSet<>(a);
-		c.addAll(b);
-		System.out.println(c);
-		
-		//intersection (INTERSECÇÃO) SOMENTE AQUELES ELEMENTOS EM COMUM
-		Set<Integer> d = new TreeSet<>(a);
-		d.retainAll(b);
-		System.out.println(d);
-		
-		//difference (DIFERENÇA)
-		Set<Integer> e = new TreeSet<>(a);
-		e.removeAll(b);
-		System.out.println(e);
-		
+
+		Set<Product> set = new TreeSet<>();
+
+		set.add(new Product("TV", 900.0));
+		set.add(new Product("Notebook", 1200.0));
+		set.add(new Product("Tablet", 400.0));
+
+		for (Product p: set) {
+			System.out.println(p);
+		}
 	}
+
 }
---
+++ src/entities/Product.java
@@ -0,0 +1,52 @@
+package entities;
+
+import java.util.Objects;
+
+public class Product implements Comparable<Product>{
+	
+	private String name;
+	private double price;
+	
+	
+	public Product(String name, double price) {
+		this.name = name;
+		this.price = price;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	@Override
+	public int hashCode() {
+		return Objects.hash(name, price);
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Product other = (Product) obj;
+		return Objects.equals(name, other.name)
+				&& Double.doubleToLongBits(price) == Double.doubleToLongBits(other.price);
+	}
+	@Override
+	public String toString() {
+		return "Product [name=" + name + ", price=" + price + "]";
+	}
+	@Override
+	public int compareTo(Product other) {
+		return name.toUpperCase().compareTo(other.getName().toUpperCase());
+	}
+}
